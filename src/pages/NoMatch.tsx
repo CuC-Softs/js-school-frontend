718d19cbea325b40842f51f77dcb3381f902decc
zogss <yanlucasbranco@gmail.com>
2022-07-09 21:11:31 -0300

    feat: add the wildcard page

---
+++ src/pages/NoMatch.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card404 from '../components/Card404';
+
+const NoMatch: React.FC = () => {
+    return (     
+      <Card404 />
+    );
+};
+
+export default NoMatch;
